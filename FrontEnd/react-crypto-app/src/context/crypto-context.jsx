5567d066514d9acfbf9699b4df116a51f69085da
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-06-30 03:06:07 +0200

    try 4

--- FrontEnd/react-crypto-app/src/context/crypto-context.jsx
+++ FrontEnd/react-crypto-app/src/context/crypto-context.jsx
@@ -21,9 +21,6 @@ const [isAuthenticated, setIsAuthenticated] = useState(false);
 const [user, setUser] = useState(null);
 const [isInitialized, setIsInitialized] = useState(false);
 
-  localStorage.removeItem("jwt");
-  localStorage.removeItem("userData");
-
 useEffect(() =>{
   const token = localStorage.getItem('jwt');
   const savedUser = localStorage.getItem('userData')
