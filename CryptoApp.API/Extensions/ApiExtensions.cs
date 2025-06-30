1f8fa180014758ba68cbd3ca1e7762fe50d35128
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-06-30 02:52:29 +0200

    try 11

--- CryptoApp.API/Extensions/ApiExtensions.cs
+++ CryptoApp.API/Extensions/ApiExtensions.cs
@@ -18,7 +18,7 @@ namespace CryptoApp.API.Extensions
                     {
                         ValidateIssuer = false,
                         ValidateAudience = false,
-                        ValidateLifetime = true,
+                        ValidateLifetime = false,
                         ValidateIssuerSigningKey = true,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Value.SecretKey))
                     };
