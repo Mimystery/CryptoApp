20529ec47d52e9c833490543b03bd20a57b002a5
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-02 21:31:14 +0200

    Chart style

--- FrontEnd/react-crypto-app/src/components/layout/PortfolioChart.jsx
+++ FrontEnd/react-crypto-app/src/components/layout/PortfolioChart.jsx
@@ -30,7 +30,7 @@ const data = {
 };
 
     return(
-        <div style={{display: 'flex', marginBottom: '1rem', justifyContent: 'center', height: '40%'}}>
+        <div style={{display: 'flex', marginBottom: '1rem', justifyContent: 'center', height: '35%'}}>
             <Pie options={{ maintainAspectRatio: false }} data={data}></Pie>
         </div>
     )
