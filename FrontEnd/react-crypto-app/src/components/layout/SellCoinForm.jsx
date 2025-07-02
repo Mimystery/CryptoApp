ff1fb42549633866208dd86d3f011dd58b57ea7d
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-02 18:35:01 +0200

    try 6

--- FrontEnd/react-crypto-app/src/components/layout/SellCoinForm.jsx
+++ FrontEnd/react-crypto-app/src/components/layout/SellCoinForm.jsx
@@ -21,111 +21,108 @@ const {selectCoins, addCoin, user, prices} = useContext(CryptoContext)
 const [submitted, setSubmit] = useState(false)
 const coinRef = useRef()
 
-console.log(coin)
+if(submitted){
+  return (<Result
+    status="success"
+    title="Successfully added new coin to your wallet!"
+    subTitle={`Added ${coinRef.current.amount} of ${coin.name} 
+    by price ${coinRef.current.price} to your wallet`}
+    extra={[
+      <Button type="primary" key="console" onClick={onClose}>
+        Close
+      </Button>,
+    ]}
+  />)
+}
 
 
-// if(submitted){
-//   return (<Result
-//     status="success"
-//     title="Successfully added new coin to your wallet!"
-//     subTitle={`Added ${coinRef.current.amount} of ${coin.name} 
-//     by price ${coinRef.current.price} to your wallet`}
-//     extra={[
-//       <Button type="primary" key="console" onClick={onClose}>
-//         Close
-//       </Button>,
-//     ]}
-//   />)
-// }
+const onFinish = (values) =>{
+  const newCoin = {
+    coinId: coin.id,
+    symbol: coin.symbol,
+    name: coin.name,
+    imageUrl: coin.imageUrl,
+    amount: values.amount,
+    price: values.price,
+    transactionDate: values.date?.$d ?? new Date(),
+  }
+  coinRef.current = newCoin
+  addCoin(newCoin)
+  setSubmit(true)
+}
+
+const updateTotal = () => {
+  const amount = form.getFieldValue('amount') || 0
+  const price = form.getFieldValue('price') || 0
+  form.setFieldsValue({
+    total: +(amount * price).toFixed(2),
+  })
+}
+
+  return(
+    <Form
+    form={form}
+      name="basic"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 10 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{
+        price: +(prices.find(p => p.symbol.replace(/USDT$/, '').toLowerCase() === 
+      coin.symbol.toLowerCase())?.price)
+      }}
+      onFinish={onFinish}
+      validateMessages={validateMessages}>
+        <CoinLabel coin={coin}/>
+        <Divider/>
+    <Form.Item
+      label="Amount"
+      name="amount"
+      rules={[{ 
+        required: true, 
+        type: 'number',
+        min: 0,
+      }]}
+      style={{ width: '100%', marginRight: '1rem' }}
+    >
+      <InputNumber onChange={updateTotal} placeholder="Enter coin amount" style={{ width: '100%' }} 
+      parser={(value) => value.replace(',', '.').replace(/[^\d.]/g, '')}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Price"
+      name="price"
+      rules={[{ 
+        required: true, 
+        type: 'number',
+        min: 0,
+
+        }]}
+        style={{ width: '100%'}}
+    >
+      <InputNumber onChange={updateTotal} placeholder="Enter coin price" style={{ width: '100%' }} step={0.01}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Date"
+      name="date"
+        style={{ width: '100%' }}
+    >
+      <DatePicker showTime style={{ width: '100%' }}/>
+    </Form.Item>
+
+    <Form.Item
+      label="Total"
+      name="total"
+    >
+      <InputNumber disabled style={{ width: '100%' }}/>
+    </Form.Item>
 
 
-// const onFinish = (values) =>{
-//   const newCoin = {
-//     coinId: coin.id,
-//     symbol: coin.symbol,
-//     name: coin.name,
-//     imageUrl: coin.imageUrl,
-//     amount: values.amount,
-//     price: values.price,
-//     transactionDate: values.date?.$d ?? new Date(),
-//   }
-//   coinRef.current = newCoin
-//   addCoin(newCoin)
-//   setSubmit(true)
-// }
-
-// const updateTotal = () => {
-//   const amount = form.getFieldValue('amount') || 0
-//   const price = form.getFieldValue('price') || 0
-//   form.setFieldsValue({
-//     total: +(amount * price).toFixed(2),
-//   })
-// }
-
-//   return(
-//     <Form
-//     form={form}
-//       name="basic"
-//       labelCol={{ span: 4 }}
-//       wrapperCol={{ span: 10 }}
-//       style={{ maxWidth: 600 }}
-//       initialValues={{
-//         price: +(prices.find(p => p.symbol.replace(/USDT$/, '').toLowerCase() === 
-//       coin.symbol.toLowerCase())?.price)
-//       }}
-//       onFinish={onFinish}
-//       validateMessages={validateMessages}>
-//         <CoinLabel coin={coin}/>
-//         <Divider/>
-//     <Form.Item
-//       label="Amount"
-//       name="amount"
-//       rules={[{ 
-//         required: true, 
-//         type: 'number',
-//         min: 0,
-//       }]}
-//       style={{ width: '100%', marginRight: '1rem' }}
-//     >
-//       <InputNumber onChange={updateTotal} placeholder="Enter coin amount" style={{ width: '100%' }} 
-//       parser={(value) => value.replace(',', '.').replace(/[^\d.]/g, '')}/>
-//     </Form.Item>
-
-//     <Form.Item
-//       label="Price"
-//       name="price"
-//       rules={[{ 
-//         required: true, 
-//         type: 'number',
-//         min: 0,
-
-//         }]}
-//         style={{ width: '100%'}}
-//     >
-//       <InputNumber onChange={updateTotal} placeholder="Enter coin price" style={{ width: '100%' }} step={0.01}/>
-//     </Form.Item>
-
-//     <Form.Item
-//       label="Date"
-//       name="date"
-//         style={{ width: '100%' }}
-//     >
-//       <DatePicker showTime style={{ width: '100%' }}/>
-//     </Form.Item>
-
-//     <Form.Item
-//       label="Total"
-//       name="total"
-//     >
-//       <InputNumber disabled style={{ width: '100%' }}/>
-//     </Form.Item>
-
-
-//     <Form.Item>
-//       <Button type="primary" htmlType="submit">
-//         Submit
-//       </Button>
-//     </Form.Item>
-//   </Form>
-//   )
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  )
 }
