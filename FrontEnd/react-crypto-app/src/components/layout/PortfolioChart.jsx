556c8d819ea29e997ab01fee2ab212fdc4b13bf8
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-02 02:51:46 +0200

    try 10

--- FrontEnd/react-crypto-app/src/components/layout/PortfolioChart.jsx
+++ FrontEnd/react-crypto-app/src/components/layout/PortfolioChart.jsx
@@ -15,7 +15,7 @@ const data = {
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: wallet.map(c => c.totalAmount),
       backgroundColor: [
         'rgba(255, 99, 132, 1)',
         'rgba(54, 162, 235, 1)',
