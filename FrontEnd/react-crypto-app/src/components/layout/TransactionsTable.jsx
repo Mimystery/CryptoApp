60b63c6733582737acb6798bcf1b4f6d5b94c3f2
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-02 03:48:15 +0200

    try 4

--- FrontEnd/react-crypto-app/src/components/layout/TransactionsTable.jsx
+++ FrontEnd/react-crypto-app/src/components/layout/TransactionsTable.jsx
@@ -7,94 +7,31 @@ const columns = [
     title: 'Name',
     dataIndex: 'name',
     showSorterTooltip: { target: 'full-header' },
-    filters: [
-      {
-        text: 'Joe',
-        value: 'Joe',
-      },
-      {
-        text: 'Jim',
-        value: 'Jim',
-      },
-      {
-        text: 'Submenu',
-        value: 'Submenu',
-        children: [
-          {
-            text: 'Green',
-            value: 'Green',
-          },
-          {
-            text: 'Black',
-            value: 'Black',
-          },
-        ],
-      },
-    ],
- 
-    onFilter: (value, record) => record.name.indexOf(value) === 0,
+
     sorter: (a, b) => a.name.length - b.name.length,
     sortDirections: ['descend'],
   },
   {
-    title: 'Price',
+    title: 'Price, $',
     dataIndex: 'price',
     defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age,
+    sorter: (a, b) => a.price - b.price,
   },
   {
     title: 'Amount',
     dataIndex: 'amount',
     defaultSortOrder: 'descend',
-    sorter: (a, b) => a.age - b.age,
+    sorter: (a, b) => a.amount - b.amount,
   },
   {
     title: 'Transaction date',
     dataIndex: 'transactionDate',
-    filters: [
-      {
-        text: 'London',
-        value: 'London',
-      },
-      {
-        text: 'New York',
-        value: 'New York',
-      },
-    ],
+
+    sorter: (a, b) => a.transactionDate - b.transactionDate,
     onFilter: (value, record) => record.address.indexOf(value) === 0,
   },
   
 ];
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sydney No. 1 Lake Park',
-  },
-  {
-    key: '4',
-    name: 'Jim Red',
-    age: 32, 
-    address: 'London No. 2 Lake Park',
-  },
-];
-
-const onChange = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
-};
 
 export default function TransactionsTable(){
 const {wallet, prices, transactions} = useContext(CryptoContext)
@@ -104,7 +41,13 @@ const data = transactions.map((c) => ({
     name: c.name,
     price: c.price,
     amount: c.amount,
-    transactionDate: c.transactionDate,
+    transactionDate: new Date(c.transactionDate).toLocaleString('uk-UA', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }),
 }))
 
 const onChange = (pagination, filters, sorter, extra) => {
