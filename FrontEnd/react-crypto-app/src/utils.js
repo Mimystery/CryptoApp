27e005503d81f14943634dfacc874bc6311048b6
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-03 00:24:19 +0200

    Very final commit

--- FrontEnd/react-crypto-app/src/utils.js
+++ FrontEnd/react-crypto-app/src/utils.js
@@ -6,14 +6,13 @@ export const percentDifference = (a, b) => {
 export const isTokenExpired = (token) => {
     try {
         const payload = JSON.parse(atob(token.split('.')[1]));
-        const exp = payload.exp * 1000; // ms
-        const now = Date.now(); // тоже в ms
+        const exp = payload.exp * 1000; 
+        const now = Date.now(); 
 
         const secondsLeft = Math.floor((exp - now) / 1000);
-        console.log('⏱ Time until token expiration:', secondsLeft, 'seconds');
 
         return now > exp;
     } catch (e) {
-        return true; // если невалидный токен — считаем его просроченным
+        return true; 
     }
 };
