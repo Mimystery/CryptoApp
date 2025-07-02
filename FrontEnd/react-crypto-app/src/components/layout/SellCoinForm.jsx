704dd56e9996572cac2b75b98cac4cb7b927758e
Vlad Kudyrko <vladkudyrko@hotmail.com>
2025-07-02 18:29:18 +0200

    try 3

--- FrontEnd/react-crypto-app/src/components/layout/SellCoinForm.jsx
+++ FrontEnd/react-crypto-app/src/components/layout/SellCoinForm.jsx
@@ -21,127 +21,111 @@ const {selectCoins, addCoin, user, prices} = useContext(CryptoContext)
 const [submitted, setSubmit] = useState(false)
 const coinRef = useRef()
 
-if(submitted){
-  return (<Result
-    status="success"
-    title="Successfully added new coin to your wallet!"
-    subTitle={`Added ${coinRef.current.amount} of ${coin.name} 
-    by price ${coinRef.current.price} to your wallet`}
-    extra={[
-      <Button type="primary" key="console" onClick={onClose}>
-        Close
-      </Button>,
-    ]}
-  />)
-}
+console.log(coin)
 
-// if(!coin){
-//     return(
-//         <Select
-//     style={{ width: '100%' }}
-//     onSelect={(v) => setCoin(selectCoins.find((c) => c.symbol === v))}
-//     value="Select coin"
-//     options={selectCoins.map((coin)=>({
-//       label: coin.name,
-//       value: coin.symbol,
-//       icon: coin.imageUrl
-//     }))}
-//     optionRender={option => (
-//       <Space>
-//         <img src={option.data.icon} alt={option.data.label} style={{width: 20}}/> {option.data.label}
-//       </Space>
-//     )}
-//   />
-//     )
+
+// if(submitted){
+//   return (<Result
+//     status="success"
+//     title="Successfully added new coin to your wallet!"
+//     subTitle={`Added ${coinRef.current.amount} of ${coin.name} 
+//     by price ${coinRef.current.price} to your wallet`}
+//     extra={[
+//       <Button type="primary" key="console" onClick={onClose}>
+//         Close
+//       </Button>,
+//     ]}
+//   />)
 // }
 
-const onFinish = (values) =>{
-  const newCoin = {
-    coinId: coin.id,
-    symbol: coin.symbol,
-    name: coin.name,
-    imageUrl: coin.imageUrl,
-    amount: values.amount,
-    price: values.price,
-    transactionDate: values.date?.$d ?? new Date(),
-  }
-  coinRef.current = newCoin
-  addCoin(newCoin)
-  setSubmit(true)
-}
 
-const updateTotal = () => {
-  const amount = form.getFieldValue('amount') || 0
-  const price = form.getFieldValue('price') || 0
-  form.setFieldsValue({
-    total: +(amount * price).toFixed(2),
-  })
-}
+// const onFinish = (values) =>{
+//   const newCoin = {
+//     coinId: coin.id,
+//     symbol: coin.symbol,
+//     name: coin.name,
+//     imageUrl: coin.imageUrl,
+//     amount: values.amount,
+//     price: values.price,
+//     transactionDate: values.date?.$d ?? new Date(),
+//   }
+//   coinRef.current = newCoin
+//   addCoin(newCoin)
+//   setSubmit(true)
+// }
 
-  return(
-    <Form
-    form={form}
-      name="basic"
-      labelCol={{ span: 4 }}
-      wrapperCol={{ span: 10 }}
-      style={{ maxWidth: 600 }}
-      initialValues={{
-        price: +(prices.find(p => p.symbol.replace(/USDT$/, '').toLowerCase() === 
-      coin.symbol.toLowerCase())?.price)
-      }}
-      onFinish={onFinish}
-      validateMessages={validateMessages}>
-        <CoinLabel coin={coin}/>
-        <Divider/>
-    <Form.Item
-      label="Amount"
-      name="amount"
-      rules={[{ 
-        required: true, 
-        type: 'number',
-        min: 0,
-      }]}
-      style={{ width: '100%', marginRight: '1rem' }}
-    >
-      <InputNumber onChange={updateTotal} placeholder="Enter coin amount" style={{ width: '100%' }} 
-      parser={(value) => value.replace(',', '.').replace(/[^\d.]/g, '')}/>
-    </Form.Item>
+// const updateTotal = () => {
+//   const amount = form.getFieldValue('amount') || 0
+//   const price = form.getFieldValue('price') || 0
+//   form.setFieldsValue({
+//     total: +(amount * price).toFixed(2),
+//   })
+// }
 
-    <Form.Item
-      label="Price"
-      name="price"
-      rules={[{ 
-        required: true, 
-        type: 'number',
-        min: 0,
+//   return(
+//     <Form
+//     form={form}
+//       name="basic"
+//       labelCol={{ span: 4 }}
+//       wrapperCol={{ span: 10 }}
+//       style={{ maxWidth: 600 }}
+//       initialValues={{
+//         price: +(prices.find(p => p.symbol.replace(/USDT$/, '').toLowerCase() === 
+//       coin.symbol.toLowerCase())?.price)
+//       }}
+//       onFinish={onFinish}
+//       validateMessages={validateMessages}>
+//         <CoinLabel coin={coin}/>
+//         <Divider/>
+//     <Form.Item
+//       label="Amount"
+//       name="amount"
+//       rules={[{ 
+//         required: true, 
+//         type: 'number',
+//         min: 0,
+//       }]}
+//       style={{ width: '100%', marginRight: '1rem' }}
+//     >
+//       <InputNumber onChange={updateTotal} placeholder="Enter coin amount" style={{ width: '100%' }} 
+//       parser={(value) => value.replace(',', '.').replace(/[^\d.]/g, '')}/>
+//     </Form.Item>
 
-        }]}
-        style={{ width: '100%'}}
-    >
-      <InputNumber onChange={updateTotal} placeholder="Enter coin price" style={{ width: '100%' }} step={0.01}/>
-    </Form.Item>
+//     <Form.Item
+//       label="Price"
+//       name="price"
+//       rules={[{ 
+//         required: true, 
+//         type: 'number',
+//         min: 0,
 
-    <Form.Item
-      label="Date"
-      name="date"
-        style={{ width: '100%' }}
-    >
-      <DatePicker showTime style={{ width: '100%' }}/>
-    </Form.Item>
+//         }]}
+//         style={{ width: '100%'}}
+//     >
+//       <InputNumber onChange={updateTotal} placeholder="Enter coin price" style={{ width: '100%' }} step={0.01}/>
+//     </Form.Item>
 
-    <Form.Item
-      label="Total"
-      name="total"
-    >
-      <InputNumber disabled style={{ width: '100%' }}/>
-    </Form.Item>
+//     <Form.Item
+//       label="Date"
+//       name="date"
+//         style={{ width: '100%' }}
+//     >
+//       <DatePicker showTime style={{ width: '100%' }}/>
+//     </Form.Item>
+
+//     <Form.Item
+//       label="Total"
+//       name="total"
+//     >
+//       <InputNumber disabled style={{ width: '100%' }}/>
+//     </Form.Item>
 
 
-    <Form.Item>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-  )
+//     <Form.Item>
+//       <Button type="primary" htmlType="submit">
+//         Submit
+//       </Button>
+//     </Form.Item>
+//   </Form>
+//   )
 }
